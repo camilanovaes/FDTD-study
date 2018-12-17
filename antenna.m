3940fe24144f56b0fcc6e9c3025754410ee69f6a
Camila Novaes <novaes.csw@gmail.com>
2018-12-16 21:40:24 -0300

    Inicial commit

---
+++ antenna.m
@@ -0,0 +1,19 @@
+rectangle('Position',[94, 120, 12, 40],'LineWidth',3)
+    rectangle('Position',[93, 118, 14, 2],'LineWidth',3)
+    rectangle('Position',[92, 116, 16, 2],'LineWidth',3)
+    
+    rectangle('Position',[91, 114, 18, 2],'LineWidth',3)
+    rectangle('Position',[90, 112, 20, 2],'LineWidth',3)
+    rectangle('Position',[89, 110, 22, 2],'LineWidth',3)
+    
+    rectangle('Position',[88, 108, 24, 2],'LineWidth',3)
+    rectangle('Position',[87, 106, 26, 2],'LineWidth',3)
+    rectangle('Position',[86, 104, 28, 2],'LineWidth',3)
+    
+    
+    rectangle('Position',[85, 102, 30, 2],'LineWidth',3)
+    rectangle('Position',[84, 100, 32, 2],'LineWidth',3)
+    rectangle('Position',[83, 98, 34, 2],'LineWidth',3)
+    
+    rectangle('Position',[82, 96, 36, 2],'LineWidth',3)
+    rectangle('Position',[81, 94, 38, 2],'LineWidth',3)
